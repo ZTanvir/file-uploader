dcf12988218723304e7b2ebbbd6b8d2103dbf5b5
zahirul <zahirulopel@gmail.com>
2025-08-07 15:46:29 +0600

    install prisma

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.set("public", path.join(__dirname, "public"));
 app.use("/public", express.static(path.join(__dirname, "public")));
+// body parser middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.render("home");
--- routes/signup.js
+++ routes/signup.js
@@ -5,4 +5,11 @@ signupRoute.get("/sign-up", (req, res) => {
   res.render("pages/signup-page");
 });
 
+signupRoute.post("/sign-up", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.username;
+  const confirmPassword = req.body.confirmPassword;
+  console.log({ username, password, confirmPassword });
+});
+
 module.exports = signupRoute;
