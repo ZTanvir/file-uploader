2a98fbda4e1b4fb6485fc49e9c0e717b997108ea
zahirul <zahirulopel@gmail.com>
2025-08-10 16:17:57 +0600

    show login validation msg when login form are empty

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const loginRoute = express.Router();
+const { body, validationResult } = require("express-validator");
+
+const signinValidationResult = [
+  body("username").notEmpty().withMessage("Please enter your username."),
+  body("password").notEmpty().withMessage("Please enter your password."),
+];
 
 loginRoute.get("/log-in", (req, res) => {
-  res.render("pages/login-page");
+  return res.render("pages/login-page", {
+    errors: [],
+    formData: {},
+  });
+});
+
+loginRoute.post("/log-in", signinValidationResult, async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  // validate username,password,confirm password
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).render("pages/login-page", {
+      errors: errors.array(),
+      formData: { username, password },
+    });
+  }
 });
 
 module.exports = loginRoute;
